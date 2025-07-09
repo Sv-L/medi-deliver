5c83db8d78a0f7be09f8f3eb51f03458d7dc5fae
Sv-L <asl.warmhaus@gmail.com>
2025-07-09 19:34:19 +0200

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://medi-deliver.onrender.com/api';
-axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.baseURL = 'https://medi-deliver.onrender.com/api';
+// axios.defaults.baseURL = 'http://localhost:3001/api';
 
 export default axios;
