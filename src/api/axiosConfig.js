056101827a16d974b2fd033f4d072f57bbbc738f
Sv-L <asl.warmhaus@gmail.com>
2025-07-17 14:50:49 +0200

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://medi-deliver.onrender.com/api';
-axios.defaults.baseURL = 'http://localhost:3001/api';
+axios.defaults.baseURL = 'https://medi-deliver.onrender.com/api';
+// axios.defaults.baseURL = 'http://localhost:3001/api';
 // https://docs.google.com/spreadsheets/d/1ufIlG2HwqppnY1by3HD4l6TVGq4EoW_o8b-ubA-pQq4/edit?pli=1&gid=0#gid=0
 
 export default axios;
